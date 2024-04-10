bff794c26711ec53e9e2dee71585036644cca688
Aliya Shakirova <aliya.sulimova@gmail.com>
2024-04-10 10:50:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Square_Project;
+
+var square1 = new Square(5, "green");
+var square2 = new Square(2,"blue");
+var square3 = new Square(15, "lightblue");
+
+square1.GetInfo();
+square2.GetInfo();
+square3.GetInfo();
+
+square1.SideLength = square2.SideLength * 3;
+Console.WriteLine("Changed info:");
+square1.GetInfo();
---
+++ Square.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Project
+{
+    internal class Square
+    {
+        private int _sideLength;
+        private string _color;
+
+        public int SideLength
+        {
+            get { return _sideLength; }
+            set { _sideLength = value; }
+        }
+        public Square(int sideLength, string color) 
+        {
+            _sideLength = sideLength;
+            _color = color;
+        }
+        public Square(int sideLength)
+        {
+            _sideLength = sideLength;
+            _color = "black";
+        }
+        public Square()
+        {
+            _sideLength = 0;
+            _color = "transparent";
+        }
+
+        public void SquareArea()
+        {
+            int _squarearea = _sideLength * _sideLength;
+            Console.WriteLine($"The square area is {_squarearea}, the color is {_color}.");
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"This is a {_color} square which side length is {_sideLength}.");
+        }
+    }
+}
